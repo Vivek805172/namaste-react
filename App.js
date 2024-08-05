316a3d36c64e79549df069fd59b5c8dada110836
Vivek Kumar <vivek.kumar@onsolve.com>
2024-08-05 13:09:37 +0530

    learning-01

---
+++ App.js
@@ -0,0 +1,4 @@
+const compenent = React.createElement("h1",{id:"Header"},"This is my first React App.");
+console.log(compenent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(compenent);
